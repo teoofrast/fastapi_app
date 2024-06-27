58ea5e0c44459897628970f18c8f55285702d72d
teoofrast <teoofrast@gmail.com>
2024-06-27 15:56:13 +0500

    sokratil proverku

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,3 @@ def test_connect_to_database(connection_to_postgres_db):
     Проверяет создана ли запись в БД
     """
     assert len(connection_to_postgres_db.all()) != 0, "Запись не была создана"
-    assert connection_to_postgres_db.first().id >= 1, "ID пользователя равна 0"
-    assert connection_to_postgres_db.first().path == 'shared_data/7750b6b9-5c99-49f1-98ff-f0c0e5903b09_dsa2.jpeg', 'Неверный путь'
-    assert connection_to_postgres_db.first().date is not None, 'Поле дата не заполнена'
