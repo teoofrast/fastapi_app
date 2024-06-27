cb83fa0510b3b66267ce6ec14e15a5892fdceb0c
teoofrast <teoofrast@gmail.com>
2024-06-27 16:01:21 +0500

    dsadsadsadsa

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
+import os
 import pytest
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import insert, select
 
 from src.database import engine
 from src.models import Document
+
+
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -25,3 +33,13 @@ def connection_to_postgres_db():
     session.commit()
     session.close()
 
+
+@pytest.fixture()
+def image_prebuild():
+    file_content = b"Test image content"
+    file_name = "123.jpg"
+    files = {'file': (file_name, file_content, 'image/jpg')}
+    response = client.post("/upload_doc", files=files)
+    yield response
+    file_path = response.json()['path']
+    os.remove(file_path)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,4 @@
 import pytest
-import warnings
-
-from fastapi.testclient import TestClient
-
-from src.main import app
-
-client = TestClient(app)
 
 
 def test_connect_to_database(connection_to_postgres_db):
@@ -13,3 +6,12 @@ def test_connect_to_database(connection_to_postgres_db):
     Проверяет создана ли запись в БД
     """
     assert len(connection_to_postgres_db.all()) != 0, "Запись не была создана"
+
+
+def test_add_image_to_db(image_prebuild):
+    """
+    Проверка на успешное добавление картинки
+    """
+    response = image_prebuild
+    assert response.status_code == 200
+    assert response.json()['message'] == "Image uploaded"
