0e28efd5536e9858e4a43289fd5f5a6b957c03ed
teoofrast <teoofrast@gmail.com>
2024-07-30 21:45:04 +0500

    add comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,11 @@ import os
 import shutil
 import pytest
 
-
 from src.database import engine
 from src.models import *
 from src.main import app, session
 
 from fastapi.testclient import TestClient
-
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -47,9 +45,12 @@ def prepare_image():
     file_name = f"test_{uuid.uuid4()}.png"
     file_path = f"images/{file_name}"
     with open(file_path, 'wb') as f:
-        f.write(b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\xec\x76\x8f\x8d\x00\x00\x00\x0cIDATx\x9c\x63\x00\x00\x00\x02\x00\x01\x0c\x0e\x1e\x15\x00\x00\x00\x00IEND\xaeB`\x82")
+        f.write(
+            b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\xec\x76\x8f\x8d\x00\x00\x00\x0cIDATx\x9c\x63\x00\x00\x00\x02\x00\x01\x0c\x0e\x1e\x15\x00\x00\x00\x00IEND\xaeB`\x82")
     new_image = Document(path=file_path)
     session.add(new_image)
     session.commit()
     yield new_image
     session.close()
+
+#last commit
