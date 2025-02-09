c508c49505243db984ed280e22f6772baf686fa2
Teoofrast <teoofrast@gmail.com>
2025-02-09 21:59:38 +0500

    Добавил хендлер

--- main.py
+++ main.py
@@ -92,27 +92,7 @@ app = FastAPI(title='My API')
 user_router = APIRouter()
 
 
-async def _create_new_user(body: UserCreate) -> ShowUser:
-    async with async_session() as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            user = await user_dal.create_user(
-                name=body.name,
-                surname=body.surname,
-                email=body.email,
-            )
-            return ShowUser(
-                user_id=user.user_id,
-                name=user.name,
-                surname=user.surname,
-                email=user.email,
-                is_active=user.is_active,
-            )
 
-
-@user_router.post('/', response_model=ShowUser)
-async def create_user(body: UserCreate) -> ShowUser:
-    return await _create_new_user(body)
 
 main_api_router = APIRouter()
 
