ff83cbaeb028b6c0a9d543f5f2f33901d6b2028d
Teoofrast <teoofrast@gmail.com>
2025-02-10 08:53:51 +0500

    Добавил db/models.py

--- main.py
+++ main.py
@@ -13,17 +13,7 @@ from pydantic import BaseModel, EmailStr, field_validator
 
 
 
-Base = declarative_base()
 
-
-class User(Base):
-    __tablename__ = 'users'
-
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    is_active = Column(Boolean, default=True)
 
 
 class UserDAL(object):
