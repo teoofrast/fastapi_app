e390ffca2963211f7b8898f54831643dc839fd80
Teoofrast <teoofrast@gmail.com>
2025-02-10 08:55:06 +0500

    Добавил db/dals.py

---
+++ db/dals.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import User
+
+
+class UserDAL(object):
+    """Слой для взаимодействия с моделью пользователя."""
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(
+        self, name: str, surname: str, email: str,
+    ) -> User:
+        new_user = User(
+            name=name,
+            surname=surname,
+            email=email,
+        )
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
--- main.py
+++ main.py
@@ -16,22 +16,7 @@ from pydantic import BaseModel, EmailStr, field_validator
 
 
 
-class UserDAL(object):
-    """Слой для взаимодействия с моделью пользователя."""
-    def __init__(self, db_session: AsyncSession):
-        self.db_session = db_session
 
-    async def create_user(
-        self, name: str, surname: str, email: str,
-    ) -> User:
-        new_user = User(
-            name=name,
-            surname=surname,
-            email=email,
-        )
-        self.db_session.add(new_user)
-        await self.db_session.flush()
-        return new_user
 
 
 
